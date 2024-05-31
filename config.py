ae52ee4e2a2c1c23075c87c36e8dbb2af20f6757
Banna9898 <68413142+Banna9898@users.noreply.github.com>
2024-05-31 12:11:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 27498866
-api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
-bot_token = "6528420500:AAH0-q4m1mytX1rLdG2x-qJkPBdofzHo3fc"
-auth_users = "6488911325"
+api_id = 29141829
+api_hash = "25020d00fbcfd406fc9979d24d761bff"
+bot_token = "7359097095:AAFyVnL1f2NTexUqOQ2OyRCObFMTdg4IY5Y"
+auth_users = "5323404314"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6488911325"
+osowner_users = "5323404314"
 owner_users = [int(num) for num in osowner_users.split(",")]
